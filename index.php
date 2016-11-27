64a0d595a96635fda9a6ae954bdd7249a5147059
Christopher McCullough <mcculloughjchris@gmail.com>
2016-11-27 11:55:52 -0500

    starting templating

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<?php include 'includes/include.php'; ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>netty</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	</head>
-	<body>
+<?php include 'includes/include.php';
 
-		<div class="container-fluid">
-			<div class="row">
-			</div>
-		</div>
+$content = <<<HTML
+<p>test</p>
+HTML;
 
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	</body>
-</html>
+include 'includes/template.php';
+?>
