9dc907837c601f1f5578bd3fd37c73875789bd48
Christopher McCullough <mcculloughjchris@gmail.com>
2016-11-27 12:06:17 -0500

    added css file, sidebar

--- includes/template.php
+++ includes/template.php
@@ -3,8 +3,14 @@
 	<head>
 		<title>netty</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="<?=$CONF['url'];?>css/styles.css" />
 	</head>
 	<body>
+
+		<div id="sidebar">
+			<h2><?=$CONF['name'];?></h2>
+			<a href="#">home</a>
+		</div>
 
 		<div class="container-fluid">
 			<?=$content;?>
