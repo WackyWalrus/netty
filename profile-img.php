0b6e4fc9cd726332ca122239c864f72a2dc60169
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-11 14:18:45 -0500

    made MikeWazowski the default profile pic, fixed profile pic code

--- profile-img.php
+++ profile-img.php
@@ -2,23 +2,23 @@
 
 include 'includes/include.php';
 
-$default_pic = "{$CONF['url']}images/default.jpg";
+$default_pic = "{$CONF['url']}images/default.jpeg";
 $id = intval($_GET['id']);
 
-header('Content-Length: '.filesize($fn));
-header('Content-Type: image/png');
+header('Content-Type: image/jpeg');
 
 if (!$id) {
 	$img = $default_pic;
 } else {
 	$glob = glob("{$CONF['dir']}images/users/{$id}.*");
-	if (strlen($glob[0])) {
+	if (!empty($glob) && strlen($glob[0])) {
 		$img = str_replace($CONF['dir'], $CONF['url'], $glob[0]);
+	} else {
+		$img = $default_pic;
 	}
 }
 
 if (isset($img) && strlen($img)) {
-	header('Content-Length: ' . filesize($img));
 	print file_get_contents($img);
 }
 
