5fb5c75a943240ab7ab29e2d1925e718508631ed
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-12 11:07:47 -0500

    added pub/sub

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,36 @@ var app = {};
     app.elems = {};
 
     function initDOM() {
-    	app.elems.$body = $('body');
-    	app.elems.$sidebar = app.elems.$body.find('#sidebar');
-    	app.elems.$main = app.elems.$body.find('#main');
+        app.elems.$body = $('body');
+        app.elems.$sidebar = app.elems.$body.find('#sidebar');
+        app.elems.$main = app.elems.$body.find('#main');
     }
+
+    app.events = {
+        topics: {},
+        subscribe: function (topic, listener) {
+            if (!app.events.topics.hasOwnProperty(topic)) {
+                app.events.topics[topic] = [];
+            }
+
+            cache.index = app.events.topics[topic].push(listener) -1;
+
+            return {
+                remove: function () {
+                    delete app.events.topics[topic][cache.index];
+                }
+            };
+        },
+        publish: function (topic, info) {
+            if (!app.events.topics.hasOwnProperty(topic)) {
+                return;
+            }
+
+            app.events.topics[topic].forEach(function (item) {
+                item(info !== undefined ? info : {});
+            });
+        }
+    };
 
     app.init = function () {
         initDOM();
