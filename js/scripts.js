6f6d8c400f09ebbc4fc7fac85818b174a58b1f60
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-15 14:32:48 -0500

    didn't like pub/sub, re-doing

--- js/scripts.js
+++ js/scripts.js
@@ -16,37 +16,9 @@ var app = {};
         // do nothing for now
     }
 
-    app.events = {
-        topics: {},
-        subscribe: function (topic, listener) {
-            if (!app.events.topics.hasOwnProperty(topic)) {
-                app.events.topics[topic] = [];
-            }
-
-            cache.index = app.events.topics[topic].push(listener) -1;
-
-            return {
-                remove: function () {
-                    delete app.events.topics[topic][cache.index];
-                }
-            };
-        },
-        publish: function (topic, info) {
-            if (!app.events.topics.hasOwnProperty(topic)) {
-                return;
-            }
-
-            app.events.topics[topic].forEach(function (item) {
-                item(info !== undefined ? info : {});
-            });
-        }
-    };
-
     app.init = function () {
         initCache();
         initDOM();
-
-        console.log(app);
     };
 
 }(app, $));
