198ee9d68d4b195bf60d553c933edd7e4aa95798
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-15 13:40:08 -0500

    made profile pic generator work for any file type

--- profile-img.php
+++ profile-img.php
@@ -2,21 +2,23 @@
 
 include 'includes/include.php';
 
+$mime = 'images/jpeg';
 $default_pic = "{$CONF['url']}images/default.jpeg";
 $id = intval($_GET['id']);
-
-header('Content-Type: image/jpeg');
 
 if (!$id) {
 	$img = $default_pic;
 } else {
 	$glob = glob("{$CONF['dir']}images/users/{$id}.*");
 	if (!empty($glob) && strlen($glob[0])) {
+		$mime = mime_content_type($glob[0]);
 		$img = str_replace($CONF['dir'], $CONF['url'], $glob[0]);
 	} else {
 		$img = $default_pic;
 	}
 }
+
+header("Content-Type: {$mime}");
 
 if (isset($img) && strlen($img)) {
 	print file_get_contents($img);
