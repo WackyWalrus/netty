2853ea56b065a50130e8b147a1a3da384a984917
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-15 14:57:21 -0500

    not tested, but should work fine

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,32 @@ var app = {};
     var cache = {};
     app.elems = {};
 
+    app.events = {
+        threads: [],
+        subscribe: function (thread, callback) {
+            // thread = element/event
+            var t = thread.split('/');
+
+            if (app.events.threads[t[0]] === undefined) {
+                app.events.threads[t[0]] = {};
+            }
+
+            if (app.events.threads[t[0]][t[1]] === undefined) {
+                app.events.threads[t[0]][t[1]] = callback;
+            }
+        },
+        publish: function (thread) {
+            var t = thread.split('/');
+
+            if (app.events.threads[t[0]] !== undefined) {
+                if (app.events.threads[t[0]][t[1]] !== undefined &&
+                        typeof app.events.threads[t[0]][t[1]] === 'function') {
+                    app.events.threads[t[0]][t[1]]();
+                }
+            }
+        }
+    };
+
     function initCache() {
         app.elems.$body = $('body');
         app.elems.$sidebar = app.elems.$body.find('#sidebar');
