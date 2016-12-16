832182a2aec0eedd025696cca1a002d166b15215
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-16 09:27:16 -0500

    textarea focus/blur events

--- js/scripts.js
+++ js/scripts.js
@@ -43,12 +43,23 @@ var app = {};
     }
 
     function initEvents() {
-        app.events.subscribe('/post-status__textarea', function () {
-            console.log('focus');
+        app.events.subscribe('post-status__textarea/focus', function () {
+            if (cache.$postStatusTextarea === undefined) {
+                cache.$postStatusTextarea = app.elems.$body.find('.post-status textarea');
+            }
+            cache.$postStatusTextarea.addClass('focused');
+        });
+        app.events.subscribe('post-status__textarea/blur', function () {
+            if (cache.$postStatusTextarea.val().length === 0) {
+                cache.$postStatusTextarea.removeClass('focused');
+            }
         });
 
         app.elems.$body.on('focus', '.post-status textarea', function () {
-            app.events.publish('/post-status__textarea/focus');
+            app.events.publish('post-status__textarea/focus');
+        });
+        app.elems.$body.on('blur', '.post-status textarea', function () {
+            app.events.publish('post-status__textarea/blur');
         });
     }
 
