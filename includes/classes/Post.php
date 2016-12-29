54ad8474727a3d11a917dc02d8997125f2653b84
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-29 14:55:15 -0500

    added html() to Post class

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -18,6 +18,7 @@ class Post {
 		$this->datestamp = $result[0]['datestamp'];
 
 		$this->user = new User($this->uid);
+		$this->likes();
 	}
 
 	function likes() {
@@ -32,6 +33,31 @@ class Post {
 
 		$this->likes = $result[0]['COUNT(*)'];
 		return $this->likes;
+	}
+
+	function html() {
+		$user = $this->user;
+		$date = Utils::EpochToDateTime($this->datestamp);
+		$content = nl2br($this->post);
+		$s = ($this->likes !== 1) ? 's' : '';
+		$html = <<<HTML
+<div class="post" id="post-$this->id">
+	<div class="post__user-info">
+		<a href="$user->url"><img src="$user->picUrl" width="50" height="50" class="post__user-img img-circle" /></a>
+		<div class="post__user-info__text">
+			<div class="post__user-info__name"><a href="$user->url">$user->username</a></div>
+			<div class="post__user-info__datestamp">$date</div>
+		</div>
+	</div>
+	<div class="post__content">$content</div>
+	<div class="post__options">
+		<a href="#" class="action-like"><span class="num">$this->likes</span> Like<span class="s">$s</span></a> 
+		<a href="#">Comment</a> 
+		<a href="#">Share</a>
+	</div>
+</div>
+HTML;
+		return $html;
 	}
 }
 
