21fcbb05c1e70498dc7b2028a0db779bd5a4e208
Christopher McCullough <mcculloughjchris@gmail.com>
2017-01-03 09:39:22 -0500

    close overlay functionality

--- js/app.utils.js
+++ js/app.utils.js
@@ -12,6 +12,11 @@ if (app === undefined) {
         init: function () {
         },
         overlay: function (content) {
+            function closeOverlay() {
+                cache.$overlay.off('click', '.overlay-close', closeOverlay);
+                cache.$overlay.remove();
+            }
+
             if (content !== undefined) {
                 cache.$overlay = $('<div class="overlay-backdrop"><div class="overlay-content"><div class="overlay-close">X</div>' + content + '</div></div>');
                 app.elems.$body.append(cache.$overlay);
@@ -19,6 +24,7 @@ if (app === undefined) {
                     'margin-left': -(cache.$overlay.find('.overlay-content').width() / 2),
                     'margin-top': -(cache.$overlay.find('.overlay-content').height() / 2)
                 });
+                cache.$overlay.on('click', '.overlay-close', closeOverlay);
                 return true;
             }
             return false;
